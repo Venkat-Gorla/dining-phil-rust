9c2f638bf5dbc8fd06f444fd49aeef7ef2ac8dd0
Venkat Gorla <venkatagorla@gmail.com>
2025-05-24 11:41:40 +0530

    [Scaffold for ForkPool struct] that threads can leverage Pending: client code and unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::sync::{Arc, Mutex};
+
+pub struct ForkPool {
+    forks: Vec<Arc<Mutex<i32>>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl ForkPool {
+    pub fn new(num_forks: usize) -> Self {
+        let forks = (0..num_forks)
+            .map(|_| Arc::new(Mutex::new(0)))
+            .collect();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Self { forks }
+    }
+
+    pub fn get_fork_pair(&self, index: usize) -> (Arc<Mutex<i32>>, Arc<Mutex<i32>>) {
+        let left_fork = self.forks[index].clone();
+        let right_fork = self.forks[(index + 1) % self.forks.len()].clone();
+        (left_fork, right_fork)
     }
 }
